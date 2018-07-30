55d8b4b0971e62efdc4e2613faf62f2b954d474f
Matt Tilley <mtilley255@gmail.com>
2018-07-30 09:47:43 -0500

    Set up remainder of basic template structure / tested and working.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Second from "./Second.vue"
+import Third from "./Third.vue"
 
 new Vue({
   el: '#app',
@@ -11,5 +12,14 @@ new Vue({
 
   el: "#something",
   render: h => h(Second)
+
+})
+
+
+new Vue({
+
+  el: "#anotherThing",
+  render: h => h(Third)
+
 
 })
